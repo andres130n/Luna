f3e756b71c2789e7ea1a8a9b6fbba5f3499b6c62
Andres Leon P <andres130n@gmail.com>
2023-12-01 15:37:51 -0500

    terminancion de proyecto
    fin de sellecion de persona

--- seleccionPersona.php
+++ seleccionPersona.php
@@ -1,159 +1,118 @@
-<!doctype html>
-<meta http-equiv=”Content-Type” content=”text/html; charset=ISO-8859-1″ />
 <?php
   session_start();
   if(!isset($_SESSION["user_id"]) || $_SESSION["user_id"]==null){
     print "<script>alert(\"Acceso invalido!\");window.location='login.php';</script>";
   }
 ?>
-<html class="no-js" lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Consulta de Examenes</title>
-<link rel="stylesheet" href="css/foundation.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/normalize.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/foundation.min.css">
-<link href='https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css' rel='stylesheet' type='text/css'>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-</head>
-<body>
 
-  <div class="row">
-    <br>
-    <?php include "php/navbar.php"; ?>
+<!DOCTYPE html>
 
-    <div class="large-12 columns">
-      <h4><strong>Usuario: </strong>
-        <?php
-        echo $_SESSION["user_id"];
-        ?>
-    </h4>
-      </div>
-  </div>
+<html lang="en">
+    <head>
 
-  <!--Seleccion del cliente --> 
-  <div class="row">
-      <div class="large-12 columns">
-        <table class='table table-hover' id='dev-table'>
-        <tr> 
-          <th scope='col'>PDF</th>   
-          <th scope='col'>Paciente</th>  
-          <th scope='col'>Fecha</th>  
-          <th scope='col'>Documento</th>  
-        </tr>  
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <title>Mesa de trabajo</title>
+        <script src="https://kit.fontawesome.com/fa20bd5746.js" crossorigin="anonymous"></script>
+        <meta name="generator" content="Bootply" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    		  <?php
+        <link rel="shortcut icon" href="../webroot/img/server.ico" type="image/ico"  />
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
-          $post = (isset($_POST['identificacion']));
+        <!--[if lt IE 9]>
+                <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <link href="css/styles.css" rel="stylesheet">
+    </head>
+    <body>
 
-          if ( $post ) {
-
-            include "php/conexion.php";
-
-            $filtro = $_POST["identificacion"];
-
-      			$sql= " SELECT DISTINCT mnDocumentNumber, szNameAlpha, jdDateInvoice, mnTimeofDay,  szUserID  
-            FROM t57011z1 WHERE szDocumentType = 'PU' AND mnCodCliente = '".$filtro."' group by mnDocumentNumber order by jdDateInvoice desc
-              ";
-           
-      			$query = $con->query($sql);
-
-      			echo "<tbody>";  
-
-      				while ($row=$query->fetch_array()) {
-                echo "<tr>";  
-                  echo "<td><a href='https://d3veloperstudio.com/laboratoriobt/EXAMEN".$row[0].".pdf' download='".$row[1]."_".$row[0].".pdf'>DESCARGAR</a></td>";                   
-                  echo "<td>".$row[1]."</td>";  
-                  echo "<td>".$row[2]."-".$row[3]."</td>";  
-
-                  echo "<td>".$row[0]."</td>";  
-                  echo "</tr>"; 
-      				}
-
-            }else{
-              echo "Persona no seleccionada.";
-
-            }
-    		  ?>
-
-  			</tbody>
-  		</table>
-         
-      </div>
-  </div>
-
-    <footer class="row">
-        <div class="large-12 columns">
-          <hr>
-          <div class="row">
-            <div class="large-6 columns">
-              <p>© Copyright Luna developed with <3</p>
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Laboratorio</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-center ">
+                      <li class="nav-item"> <a class="nav-link active" href="mesatrabajo.php">Principal</a> </li>
+                      <li class="nav-item"><a class="nav-link " href="./php/logout.php">Salir</a></li>
+                    </ul>
+                </div>
             </div>
-            <div class="large-6 columns">
-            <ul class="inline-list right">
+        </nav>
 
-            </ul>
+        <div class="container-fluid">
+
+            <div class="row row-offcanvas row-offcanvas-left">
+
+
+
+                <div class="main">
+
+                    <!--toggle sidebar button-->
+                    <p class="visible-xs">
+                        <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas"><i class="glyphicon glyphicon-chevron-left"></i></button>
+                    </p>
+
+                    <h3>Busca tu examen </h3>
+                   
+                    <table class='table table-hover' id='dev-table'>
+                      <tr> 
+                        <th scope='col'>PDF</th>   
+                        <th scope='col'>Paciente</th>  
+                        <th scope='col'>Fecha</th>  
+                        <th scope='col'>Documento</th>  
+                      </tr>  
+                        <?php
+                        $post = (isset($_POST['identificacion']));
+                        if ( $post ) {
+                          include "php/conexion.php";
+                          $filtro = $_POST["identificacion"];
+                          $sql= " SELECT DISTINCT mnDocumentNumber, szNameAlpha, jdDateInvoice, mnTimeofDay,  szUserID  
+                          FROM t57011z1 WHERE szDocumentType = 'PU' AND mnCodCliente = '".$filtro."' group by mnDocumentNumber order by jdDateInvoice desc
+                            ";
+                        
+                          $query = $con->query($sql);
+                          echo "<tbody>";  
+                            while ($row=$query->fetch_array()) {
+                              echo "<tr>";  
+                                echo "<td><a href='https://d3veloperstudio.com/laboratoriobt/EXAMEN".$row[0].".pdf' download='".$row[1]."_".$row[0].".pdf'>DESCARGAR</a></td>";                   
+                                echo "<td>".$row[1]."</td>";  
+                                echo "<td>".$row[2]."-".$row[3]."</td>";  
+                                echo "<td>".$row[0]."</td>";  
+                                echo "</tr>"; 
+                            }
+                          }else{
+                            echo "Persona no seleccionada.";
+                          }
+                        ?>
+                      </tbody>
+                    </table>
+                    
+                   
+                   
+
+
+
+                   
+                </div><!--/row-->
             </div>
-          </div>
-        </div>
-      </footer>
+        </div><!--/.container-->
 
+        <footer>
+            <hr> 
+            <p class="pull-right"></p>
+        </footer>
 
-  <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/js/foundation.min.js"></script>
-  <script>
-        $(document).foundation();
-      </script>
-
-   <!-- jQuery search redex -->
-    <script src="js/jquery.js"></script>
-    
-    <script type="text/javascript">
-    (function(){
-        'use strict';
-        var $ = jQuery;
-        $.fn.extend({
-            filterTable: function(){
-                return this.each(function(){
-                    $(this).on('keyup', function(e){
-                        var $this = $(this), search = $this.val().toLowerCase(), target = $this.attr('data-filters'), $rows = $(target).find('tbody tr');
-                        if(search == '') {
-                            $rows.show(); 
-                        } else {
-                            $rows.each(function(){
-                                var $this = $(this);
-                                $this.text().toLowerCase().indexOf(search) === -1 ? $this.hide() : $this.show();
-                            })
-                        }
-                    });
-                });
-            }
-        });
-        $('[data-action="filter"]').filterTable();
-    })(jQuery);
-
-    $(function(){
-        // attach table filter plugin to inputs
-        $('[data-action="filter"]').filterTable();
-        
-        $('.container').on('click', '.panel-heading span.filter', function(e){
-            var $this = $(this), 
-                    $panel = $this.parents('.panel');
-            
-            $panel.find('.panel-body').slideToggle();
-            if($this.css('display') != 'none') {
-                $panel.find('.panel-body input').focus();
-            }
-        });
-        $('[data-toggle="tooltip"]').tooltip();
-    })
-
-    </script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-
-
-</body>
+        <!-- script references -->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
